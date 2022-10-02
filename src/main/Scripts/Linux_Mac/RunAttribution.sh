777c25facb364981d6be54658e2233ae7ccf73c7
harishkgithub <harish2008nw@gmai.com>
2022-10-02 22:17:25 +0530

    fix(mm):  shell script

---
+++ src/main/Scripts/Linux_Mac/RunAttribution.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Simple attribution application that produces a report!!"
+
+events_path=$1
+impressions_path=$2
+count_of_events=$3
+count_of_users=$4
+tmp_count_of_events=./tmp_count_of_events
+tmp_count_of_users=./tmp_count_of_users
+
+echo spark-submit --class com.hk.mm.assignment.AttributionApp --master local[1] \
+../../../target/scala-2.12/data-engg-challenge_2.12-0.1.0-SNAPSHOT.jar $events_path $impressions_path $tmp_count_of_events $tmp_count_of_users
+
+echo "Copying output files of count_of_events"
+cat $tmp_count_of_events/part*.csv $count_of_events
+echo "deleting temporary files of count_of_events"
+rm -r $tmp_count_of_events
+
+echo "Copying output files of count_of_users"
+cat $tmp_count_of_users/part*.csv count_of_users
+echo "deleting temporary files of count_of_users"
+rm -r $tmp_count_of_users
+
+echo "Analysis completed!!!"
